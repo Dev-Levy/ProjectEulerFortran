6a950ba0e3980cf943f7f56a219072ce99e7c59f
Olah Levente <oll4bp@bosch.com>
2025-10-02 13:49:00 +0200

    problem 2 solved

--- Problem_2/evenFibonacciSum.f90
+++ Problem_2/evenFibonacciSum.f90
@@ -1,15 +1,24 @@
 program evenFibonacciSum
     implicit none
-    
+    integer :: fib, fib_past, fib_curr, sum
     integer, parameter :: limit = 4000000
 
+    fib_past = 1
+    fib_curr = 2 
+    fib = fib_past + fib_curr
+    sum = 2
 
+    do while(fib < limit)
+        if (mod(fib,2) == 0) then
+            sum = sum + fib
+            print*, fib
+        end if 
 
+        fib_past = fib_curr
+        fib_curr = fib
+        fib = fib_past + fib_curr
+    end do
+
+    print*, '-------------------'
+    print*, sum
 end program evenFibonacciSum
-
-recursive function evenFibSum(limit) result(j)
-    integer, intent (in) :: limit
-    
-    !if fib is less than limit, call recursive again
-    !else return 
-end function evenFibSum
