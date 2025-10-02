bcfbc169b0aa4890ee6ba8b275703d9eccfcafd7
Olah Levente <oll4bp@bosch.com>
2025-10-02 10:17:36 +0200

    set up problem 2

---
+++ Problem_2/evenFibonacciSum.f90
@@ -0,0 +1,15 @@
+program evenFibonacciSum
+    implicit none
+    
+    integer, parameter :: limit = 4000000
+
+
+
+end program evenFibonacciSum
+
+recursive function evenFibSum(limit) result(j)
+    integer, intent (in) :: limit
+    
+    !if fib is less than limit, call recursive again
+    !else return 
+end function evenFibSum
